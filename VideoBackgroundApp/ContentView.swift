7618cdacea060887a9799adc24de7218180da934
pouyasadri <pouyapaghandeh78@gmail.com>
2024-01-29 20:05:31 +0100

    Initial Commit

---
+++ VideoBackgroundApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoBackgroundApp
+//
+//  Created by Pouya Sadri on 29/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoBackgroundApp/VideoBackgroundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoBackgroundAppApp.swift
+//  VideoBackgroundApp
+//
+//  Created by Pouya Sadri on 29/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoBackgroundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
